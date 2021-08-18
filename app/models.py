89c171c38dfd91a352f6890d6c4b0bf535d08e2e
SINCY JOSEPH <sincyjoseph535@gmail.com>
2021-08-18 16:47:19 +0530

    model changed

--- app/models.py
+++ app/models.py
@@ -11,12 +11,7 @@ class UserDetailsModel(models.Model):
     datepicker = models.DateField(max_length=10)
     email = models.EmailField(max_length=255)
 
-    GENDER_CHOICES = (
-        ('M', 'Male'),
-        ('F', 'Female'),
-    )
-
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=50, default='NULL')
     country = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
     file = models.FileField(upload_to='media/', default=0)
