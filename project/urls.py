ada7b10e65055e56c242cee42883a051f5bbbfcf
SINCY JOSEPH <sincyjoseph535@gmail.com>
2021-08-18 16:16:54 +0530

    staticfilechanged

--- project/urls.py
+++ project/urls.py
@@ -7,5 +7,5 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
